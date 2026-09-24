544344f2a2a9e848e6b25f0b07f80d4f3c8ff6ba
Debajyoti <debuhazra96@gmail.com>
2026-09-24 22:29:19 +0530

    add post method in api system

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from fastapi import FastAPI 
+from agent import graph
+from pydantic import BaseModel
+from langchain_core.messages import HumanMessage 
 
-app = FastAPI("API_system")
+# Conection build 
+app = FastAPI(title = "API_system")
+
+# Data Define 
+class API(BaseModel):
+    question : str 
+    user_id : int = None 
+    limit : int = 100 
+    
+# Post conection 
+@app.post("/ask")
+def asking(response:API):
+    try:
+        result = graph.invoke({
+        'messages':[HumanMessage(content=(response.question))]
+    })
+    
+        return {
+            'answer':result['messages'][-1].content
+        }
+    except Exception as e :
+        return str(e)
